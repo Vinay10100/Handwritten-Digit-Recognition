2d2a106f48c8500aadf786468c732191b003841f
Vinay Uniyal <97530517+Vinay2022@users.noreply.github.com>
2023-04-08 15:58:03 +0530

    Update predict.py

--- streamlit/predict.py
+++ streamlit/predict.py
@@ -1,62 +1,57 @@
 import streamlit as st
 import tensorflow as tf
 import numpy as np
-from PIL import Image, ImageOps
+from PIL import Image
 
-# global variable to hold the TensorFlow model
-model = None
+# Load the model
+model = tf.keras.models.load_model('/streamlit/main.tflite')
 
-# function to load the model
-def load_model():
-    global model
-    model_path = 'streamlit/main.tflite'
-    model = tf.keras.models.load_model(model_path)
-
-# function to preprocess the canvas image
-def preprocess(image):
-    # resize the image to 28x28 pixels and invert the colors
-    image = ImageOps.invert(image.resize((28,28)))
-    # convert the image to grayscale and normalize the pixel values
-    image = ImageOps.grayscale(image).convert('L')
-    image_array = np.array(image) / 255.0
-    # reshape the array to have a single channel (for grayscale images)
-    image_array = image_array.reshape((1, 28, 28, 1))
+# Define function to preprocess the image
+def preprocess_image(image):
+    # Convert to grayscale
+    image = image.convert('L')
+    # Resize to 28x28 pixels
+    image = image.resize((28, 28))
+    # Invert the colors
+    image = ImageOps.invert(image)
+    # Convert to numpy array and normalize
+    image_array = np.array(image)
+    image_array = image_array / 255.0
+    # Reshape to (1, 28, 28, 1)
+    image_array = np.reshape(image_array, (1, 28, 28, 1))
     return image_array
 
-# function to predict the digit from the canvas image
-def predict_digit(image_array):
-    # predict the digit using the loaded model
-    predictions = model.predict(image_array)
-    predicted_digit = np.argmax(predictions)
-    return predicted_digit
-
-# main Streamlit code
-if __name__ == '__main__':
-    # load the model
-    load_model()
-    
-    # set up the canvas
-    st.write("Draw a digit in the canvas below:")
+# Define the Streamlit app
+def app():
+    # Set the page title
+    st.set_page_config(page_title='Handwritten Digit Recognizer')
+    # Add a title and description
+    st.title('Handwritten Digit Recognizer')
+    st.write('Draw a digit in the canvas below and click the "Predict" button to see the model prediction.')
+    # Create a canvas
     canvas_result = st_canvas(
-        fill_color="#000000",
+        fill_color='#000000',
         stroke_width=20,
-        stroke_color="#FFFFFF",
-        background_color="#000000",
+        stroke_color='#FFFFFF',
+        background_color='#000000',
         height=200,
         width=200,
-        drawing_mode="freedraw",
-        key="canvas"
+        drawing_mode='freedraw',
+        key='canvas'
     )
+    # Add a button to predict the digit
+    if st.button('Predict'):
+        # Get the canvas image data
+        image = Image.fromarray(canvas_result.image_data.astype('uint8'))
+        # Preprocess the image
+        image_array = preprocess_image(image)
+        # Make the prediction
+        prediction = model.predict(image_array)
+        # Get the predicted class label
+        predicted_class = np.argmax(prediction)
+        # Show the prediction result
+        st.write(f'Predicted digit: {predicted_class}')
 
-    # make a prediction on the drawn digit when the 'Predict' button is clicked
-    if st.button("Predict"):
-        if canvas_result.image_data is not None:
-            # get the canvas image data and preprocess it
-            canvas_image = Image.fromarray(canvas_result.image_data.astype('uint8'), 'RGBA').convert('L')
-            processed_image = preprocess(canvas_image)
-            
-            # predict the digit and display the result
-            predicted_digit = predict_digit(processed_image)
-            st.write(f"Predicted digit: {predicted_digit}")
-        else:
-            st.write("Please draw a digit first.")
+# Run the app
+if __name__ == '__main__':
+    app()

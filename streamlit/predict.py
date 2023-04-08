8bfa52b79d30061ff2a2111eb856d978a2007633
Vinay Uniyal <97530517+Vinay2022@users.noreply.github.com>
2023-04-08 17:02:22 +0530

    Update predict.py

--- streamlit/predict.py
+++ streamlit/predict.py
@@ -1,43 +1,60 @@
 import streamlit as st
 import numpy as np
+from PIL import Image
 import tensorflow as tf
-from PIL import Image, ImageOps
 
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("Handwritten Digit Recognition App")
+# Load the model
+model = tf.keras.models.load_model('streamlit/main.tflite')
 
-# Load the TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="streamlit/main.tflite")
-interpreter.allocate_tensors()
-
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-
-def predict(image):
-    # Preprocess the image.
+# Define a function to preprocess the image
+def preprocess_image(image):
+    # Resize the image to 28x28 pixels
+    image = image.resize((28, 28))
+    # Convert the image to grayscale
+    image = image.convert('L')
+    # Convert the image to a numpy array
     image = np.array(image)
-    image = image[:,:,3]
-    image = Image.fromarray((image).astype(np.uint8))
-    image = image.resize((28,28))
-    image = ImageOps.invert(image)
-    image = np.array(image)
-    image = image.reshape(1,28,28,1)
-    image = image.astype('float32')
-    image /= 255
-    
-    # Run inference on the model.
-    interpreter.set_tensor(input_details[0]['index'], image)
-    interpreter.invoke()
-    pred = interpreter.get_tensor(output_details[0]['index'])
-    return np.argmax(pred)
+    # Reshape the array to a 4D tensor with shape (1, 28, 28, 1)
+    image = image.reshape(1, 28, 28, 1)
+    # Normalize the pixel values from [0, 255] to [-1, 1]
+    image = (image - 127.5) / 127.5
+    return image
 
+# Define the main function
 def main():
-    st.write("Draw a digit from 0 to 9")
-    canvas = st.sketchpad(width=200,height=200)
-    if st.button("Predict"):
-        digit = predict(canvas.image_data)
-        st.write("Prediction:", digit)
+    # Set the title and description of the app
+    st.title("Handwritten Digit Recognition App")
+    st.write("Draw a digit from 0 to 9 on the canvas below and click the 'Predict' button to see the model's prediction.")
 
-if __name__ == "__main__":
+    # Create a canvas to draw on
+    canvas = st_canvas(
+        fill_color="#000000",
+        stroke_width=20,
+        stroke_color="#FFFFFF",
+        background_color="#000000",
+        height=200,
+        width=200,
+        drawing_mode="freedraw",
+        key="canvas"
+    )
+
+    # Get the image data from the canvas
+    if canvas.image_data is not None:
+        image = Image.fromarray(canvas.image_data.astype('uint8'), 'RGBA').convert('RGB')
+        st.image(image, caption="Your drawing", use_column_width=True)
+
+    # Add a button to predict the digit
+    if st.button('Predict'):
+        if canvas.image_data is None:
+            st.write("Please draw a digit on the canvas above.")
+        else:
+            # Preprocess the image
+            image = preprocess_image(image)
+            # Get the model's prediction
+            prediction = model.predict(image).argmax()
+            # Display the prediction
+            st.write("The model predicts that the digit is:", prediction)
+
+# Run the app
+if __name__ == '__main__':
     main()

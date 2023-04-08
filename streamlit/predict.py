ff4464ee3ea311d63dc91498dadc950261ef784d
Vinay Uniyal <97530517+Vinay2022@users.noreply.github.com>
2023-04-08 16:26:29 +0530

    Update predict.py

--- streamlit/predict.py
+++ streamlit/predict.py
@@ -1,71 +1,72 @@
-import streamlit as st
-from streamlit_canvas import st_canvas
+from keras.preprocessing import image
 import tensorflow as tf
-from PIL import Image
 import numpy as np
+import streamlit as st
+from PIL import ImageOps, Image
+import io
 
-# Load TFLite model
-model_path = 'streamlit/main.tflite'
-interpreter = tf.lite.Interpreter(model_path=model_path)
+# Load TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="streamlit/main.tflite")
 interpreter.allocate_tensors()
 
-# Define input and output details
+# Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-# Define function to preprocess input image
-def preprocess_image(image):
-    # Resize image to 28x28
+
+def predict_digit(image):
+    # Preprocess the image
+    image = ImageOps.grayscale(image)
     image = image.resize((28, 28))
-    # Convert image to grayscale
-    image = image.convert('L')
-    # Convert image to numpy array
-    image = np.array(image, dtype=np.float32)
-    # Normalize image pixels to [0, 1]
-    image /= 255.0
-    # Expand dimensions of image to match input shape of model
-    image = np.expand_dims(image, axis=0)
-    # Add batch dimension to image
-    image = np.stack([image], axis=0)
-    return image
+    image = image.reshape((1, 28, 28, 1))
+    image = np.array(image) / 255.0
 
-# Define function to make prediction
-def predict(image):
-    # Preprocess input image
-    image = preprocess_image(image)
-    # Set input tensor to preprocessed image
+    # Make a prediction using the TFLite model
     interpreter.set_tensor(input_details[0]['index'], image)
-    # Run inference
     interpreter.invoke()
-    # Get output tensor
-    output = interpreter.get_tensor(output_details[0]['index'])
-    # Get predicted label
-    label = np.argmax(output)
-    # Return predicted label
-    return label
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    prediction = np.argmax(output_data)
+    
+    return prediction
 
-# Define Streamlit app
-def app():
-    st.title("Handwritten Digit Recognition")
-    # Create canvas to draw digit
-    canvas = st_canvas(
-        fill_color="#FFFFFF",
-        stroke_width=10,
-        stroke_color="#000000",
-        background_color="#FFFFFF",
-        width=300,
-        height=300,
-        drawing_mode="freedraw",
-        key="canvas"
-    )
-    # Make prediction when "Predict" button is clicked
-    if st.button("Predict"):
-        # Convert canvas to PIL image
-        image = Image.fromarray(canvas.image_data.astype('uint8'), mode='L')
-        # Make prediction
-        label = predict(image)
-        # Display predicted label
-        st.write(f"Predicted label: {label}")
 
-if __name__ == '__main__':
-    app()
+def main():
+    st.title("Handwritten Digit Classification Web App")
+    st.set_option('deprecation.showfileUploaderEncoding', False)
+
+    activities = ["Draw", "Upload"]
+    choice = st.sidebar.selectbox("Select Activity", activities)
+
+    if choice == "Draw":
+        st.subheader("Draw Digit")
+        canvas_result = st_canvas( 
+            stroke_width=20, 
+            stroke_color="#fff", 
+            background_color="#000", 
+            height=150, 
+            width=150, 
+            drawing_mode="freedraw", 
+            key="canvas",
+        )
+        if canvas_result.image_data is not None:
+            img = ImageOps.invert(Image.fromarray(canvas_result.image_data.astype('uint8'), 'L').resize((28, 28)))
+            st.image(img)
+            if st.button("Predict"):
+                digit = predict_digit(img)
+                st.write("Predicted digit:", digit)
+
+    elif choice == "Upload":
+        st.subheader("Upload Image")
+        file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+
+        if file is not None:
+            image = Image.open(file).convert("L")
+            st.image(image, caption="Uploaded Image", use_column_width=True)
+            if st.button("Predict"):
+                digit = predict_digit(image)
+                st.write("Predicted digit:", digit)
+
+
+if __name__ == "__main__":
+    main()
+
